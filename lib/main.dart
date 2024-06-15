979549aafe7e3a222221d082047a006d77b4b566
whyageek <aryanawasthi2003@gmail.com>
2024-06-16 02:10:05 +0530

    Changes to be committed: 	modified:   lib/main.dart  Untracked files: 	assets/ 	fonts/ 	lib/models/ 	lib/screens/

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:my_app1/Pages/home.dart';
+import 'package:my_app1/screens/dashboard.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
