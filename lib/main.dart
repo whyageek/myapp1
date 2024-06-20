dbb1e62d8018432561abf1f92595064070d28970
whyageek <aryanawasthi2003@gmail.com>
2024-06-21 01:52:33 +0530

    Changes to be committed: 	deleted:    assets/icons/filter-svgrepo-com.svg 	deleted:    assets/icons/three-dots-vertical-svgrepo-com.png 	deleted:    assets/icons/three-dots-vertical-svgrepo-com.svg 	modified:   lib/main.dart 	modified:   lib/models/cigaratte.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml
     Untracked files:
    	Images/
    	lib/models/firebase.dart
    	lib/models/my_textfield.dart
    	lib/models/sq_tile.dart
    	lib/screens/Login_page.dart
    	lib/screens/auth_page.dart
    	lib/screens/selection_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:my_app1/firebase_options.dart';
+import 'package:my_app1/screens/Login_page.dart';
 import 'package:my_app1/screens/auth_page.dart';
 import 'package:my_app1/screens/home_page.dart';
-import 'package:my_app1/screens/analysis_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,30 +21,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: AuthPage(),
       theme: ThemeData(
         fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/', // Set initial route
-      routes: {
-        '/': (context) => const AuthenticationWrapper(), // Use wrapper to handle authentication logic
-        '/home': (context) => const HomePage(), // Define home route
-        '/analysis': (context) => AnalysisPage(dailyCigaretteCount: 10, monthlyCigaretteCount: 1100), // Define analysis page route
-        // Add more routes as needed
-      },
     );
-  }
-}
-
-class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    const bool isUserAuthenticated = true; 
-
-    if (isUserAuthenticated) {
-      return const AuthPage();
-    }
   }
 }
--- lib/models/cigaratte.dart
+++ lib/models/cigaratte.dart
@@ -6,20 +6,22 @@ class Cigarette {
 }
 
 List<Cigarette> cigarettes = [
-  Cigarette(name: "Gold Flake Kings", price: 20.0), // Big
-  Cigarette(name: "Gold Flake Lights", price: 20.0), // Small
-  Cigarette(name: "Classic Regular", price: 20.0), // Big
-  Cigarette(name: "Classic Milds", price: 20.0), // Small
-  Cigarette(name: "Classic Connect", price: 15.0), // Small
-  Cigarette(name: "Wills Navy Cut", price: 20.0), // Big
-  Cigarette(name: "Wills Navy Cut Small", price: 10.0), // Small
-  Cigarette(name: "Four Square Regular", price: 20.0), // Big
-  Cigarette(name: "Four Square Mild", price: 10.0), // Small
-  Cigarette(name: "Navy Cut Regular", price: 20.0), // Big
-  Cigarette(name: "Navy Cut Mild", price: 10.0), // Small
-  Cigarette(name: "Marlboro Regular", price: 20.0), // Small
-  Cigarette(name: "Marlboro Advance", price: 20.0), // Small
-  Cigarette(name: "Marlboro Compact", price: 10.0), // Small
+  Cigarette(name: "Gold Flake Kings", price: 20.0),
+  Cigarette(name: "Gold Flake Lights", price: 20.0),
+  Cigarette(name: "Classic Regular", price: 20.0), 
+  Cigarette(name: "Classic Milds", price: 20.0),
+  Cigarette(name: "Classic Connect", price: 15.0),
+  Cigarette(name: "Classic Verve", price: 15.0), 
+  Cigarette(name: "Wills Navy Cut", price: 20.0), 
+  Cigarette(name: "Wills Navy Cut Small", price: 10.0), 
+  Cigarette(name: "Four Square Regular", price: 20.0), 
+  Cigarette(name: "Four Square Mild", price: 10.0), 
+  Cigarette(name: "Navy Cut Regular", price: 20.0), 
+  Cigarette(name: "Navy Cut Mild", price: 10.0), 
+  Cigarette(name: "Marlboro Regular", price: 20.0), 
+  Cigarette(name: "Marlboro Advance", price: 20.0),
+  Cigarette(name: "Marlboro Compact", price: 10.0), 
+  Cigarette(name: "Marlboro CloveMix", price: 20.0),
   Cigarette(name: "Esse Lights", price: 10.0),
   Cigarette(name: "Esse Gold", price: 10.0),
 ];
